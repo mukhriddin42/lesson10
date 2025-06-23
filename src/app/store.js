d5ab3a21e92ad64059f597c2bd47021499e2c2f3
Muhriddin Ibragimov <imuhriddin700@gmail.com>
2025-06-23 15:39:37 +0500

    ttt

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { productApi } from "../features/products/ProductApi";
+import { productApi } from "../features/products/productApi";
 
 export const store = configureStore({
   reducer: {
